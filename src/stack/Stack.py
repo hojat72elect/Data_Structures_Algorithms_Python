b2b0161c5fe33145d68d8c19bccd11dda6343ea7
hojat72elect <hojat72elect@gmail.com>
2026-08-01 02:50:54 -0400

    Stack : Implemented the data structure.

---
+++ src/stack/Stack.py
@@ -0,0 +1,35 @@
+from typing import TypeVar, Generic
+
+from linked_list.SinglyLinkedList import SinglyLinkedList
+
+T = TypeVar("T")
+
+
+class Stack(Generic[T]):
+    def __init__(self):
+        self._list: SinglyLinkedList[T] = SinglyLinkedList[T]()
+
+    def is_empty(self) -> bool:
+        return len(self._list) == 0
+
+    def push(self, item: T):
+        self._list.prepend(item)
+
+    def pop(self) -> T:
+        if self.is_empty():
+            raise IndexError("Pop from empty stack")
+        return self._list.delete_head()
+
+    def peek(self) -> T:
+        if self.is_empty():
+            raise IndexError("Peek from empty stack")
+        return self._list.head.data
+
+    def __len__(self) -> int:
+        return len(self._list)
+
+    def __str__(self) -> str:
+        if self.is_empty():
+            return "Empty Stack"
+        items = " -> ".join(str(val) for val in self._list)
+        return f"Stack: Top -> {items}"
